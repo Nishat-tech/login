f6684679a3227a77833b169cdeb0e26ec4c68464
NishatKanwal <nishatkanwal123@gmail.com>
2025-08-22 16:46:23 +0500

    my first commit

---
+++ test/login.spec.js
@@ -0,0 +1,14 @@
+const { test, expect } = require('@playwright/test');
+
+test('Login form visible', async ({ page }) => {
+  await page.goto('file:///C:/Users/nisha/LoginDemo/login.html');
+  await page.pause
+
+  // check heading
+  await expect(page.locator('h2')).toHaveText('Login');
+
+  // check inputs
+  await expect(page.locator('#username')).toBeVisible();
+  await expect(page.locator('#password')).toBeVisible();
+  await expect(page.locator('button[type="submit"]')).toHaveText('Login');
+});
